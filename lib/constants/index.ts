57c07fb20d40834703d120e9b82347e5b8060346
Bartek Wieckowski <bart.it.pro@gmail.com>
2025-02-12 16:43:24 +0100

    refactor: Restructure app layout and improve metadata configuration

---
+++ lib/constants/index.ts
@@ -0,0 +1,7 @@
+const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'bestStore';
+const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A modern ecommerce platfrom';
+const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+
+export { APP_NAME, APP_DESCRIPTION, SERVER_URL };
