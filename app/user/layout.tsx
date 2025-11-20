ee7314b6381a4a216c6cd8e51b94fc926d847392
Bartek Wieckowski <bart.it.pro@gmail.com>
2025-11-20 11:01:10 +0100

    Refactor UserLayout to use ROUTES for navigation

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -3,6 +3,7 @@ import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 import MainNav from "./MainNav";
+import ROUTES from "@/lib/routes";
 
 export default function UserLayout({
   children,
@@ -14,7 +15,7 @@ export default function UserLayout({
       <div className="flex flex-col">
         <div className="border-b container mx-auto">
           <div className="flex items-center h-16 px-4">
-            <Link href="/" className="w-22">
+            <Link href={ROUTES.HOME} className="w-22">
               <Image
                 src="/images/logo.png"
                 height={48}
